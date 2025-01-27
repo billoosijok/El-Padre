0d707c2bd14560270301a2f3a47ef2ff1336d7e8
Belal Sejouk <billoosijok@gmail.com>
2025-01-27 04:06:30 +0100

    Coming soon ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "CNAME",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "docs",
+  },
+});
