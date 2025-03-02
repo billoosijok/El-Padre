938384d816a04f86edc11b98a4bcefa91dbdfb42
Belal Sejouk <billoosijok@gmail.com>
2025-03-02 01:52:26 +0100

    fix localStorage bug

--- docs/assets/index-Iwny839O.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index-CJWqDAfB.js";var r=a;export{r as default};
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,10 +3,10 @@ import { useCallback } from "react";
 export const useLocalStorage = () => {
   return {
     get: useCallback((key: string) => {
-      return window.sessionStorage.getItem(key);
+      return window.localStorage.getItem(key);
     }, []),
     set: useCallback((key: string, value: string) => {
-      return window.sessionStorage.setItem(key, value);
+      return window.localStorage.setItem(key, value);
     }, []),
   };
 };
