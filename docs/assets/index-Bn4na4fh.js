8832997a7cce64ffb43857c3eadde411be60bd99
Belal Sejouk <billoosijok@gmail.com>
2026-03-09 17:43:19 +0100

    Publish: Better SEO

--- docs/assets/index-Bn4na4fh.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./features-animation-C4VfRjh_.js";import"./create-visual-element-DrnwItpl.js";import"./index-CjrQpoKn.js";var m=o;export{m as default};
---
+++ docs/assets/index-ouhCBmyH.js
@@ -0,0 +1,1 @@
+import{d as o}from"./features-animation-BOhG3RQp.js";import"./create-visual-element-DYVX931z.js";import"./index-Bg_MpucQ.js";var m=o;export{m as default};
