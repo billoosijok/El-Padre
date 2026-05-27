989fd3a5d587be49564a506c806790809fd4cb53
mallgrab <mallgrab@12:83:31:d3:ae:ea.home>
2026-05-28 00:04:00 +0200

    Publish: + add brunch drinks

--- docs/assets/features-animation-DMQfkDW_.js
+++
@@ -1,1 +0,0 @@
-import{K as a,L as e,J as n}from"./create-visual-element-4WVL4ElR.js";const s={renderer:n,...e,...a};export{s as d};
--- docs/assets/index-BJ20hwqJ.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./features-animation-DMQfkDW_.js";import"./create-visual-element-4WVL4ElR.js";import"./index-BsS3jkRj.js";var m=o;export{m as default};
