d1fac35b74a38220dd4354514744fdd8c7f15517
mallgrab <mallgrab@12:83:31:d3:ae:ea.home>
2026-05-28 08:46:59 +0200

    Publish: Brunch Menu mod

--- docs/assets/index-CLdMGuH4.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./features-animation-DkArdBHP.js";import"./create-visual-element-Di3wjb-c.js";import"./index-CaXt0NuH.js";var m=o;export{m as default};
---
+++ docs/assets/index-DgYn3KKa.js
@@ -0,0 +1,1 @@
+import{d as o}from"./features-animation-B3xGLZlO.js";import"./create-visual-element-EBRChgK8.js";import"./index-CGZUGsMV.js";var m=o;export{m as default};
