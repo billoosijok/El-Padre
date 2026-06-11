2422c21b86086d667185d50795d98a22e27aaec5
mallgrab <mallgrab@a6:d9:bb:f5:da:61.home>
2026-06-11 10:09:15 +0200

    Publish: Remove language picker from the /reviews route

---
+++ docs/assets/features-animation-CvtJoZnv.js
@@ -0,0 +1,1 @@
+import{aL as a,aM as e,aN as n}from"./index-BmzmuTMR.js";const s={renderer:n,...e,...a};export{s as d};
---
+++ docs/assets/index-BmB1FW2w.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-CvtJoZnv.js";import"./index-BmzmuTMR.js";var t=a;export{t as default};
