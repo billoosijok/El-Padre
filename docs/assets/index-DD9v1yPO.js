635140bbd634f107755c305e684ff34f98153425
mallgrab <mallgrab@a6:d9:bb:f5:da:61.home>
2026-06-11 10:41:46 +0200

    Publish: fix reservation modal

---
+++ docs/assets/index-DD9v1yPO.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DNE3zgog.js";import"./index-DO15xYAG.js";var d=a;export{d as default};
--- docs/assets/index-Dv2eFTij.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-CBN4HQnJ.js";import"./index-D_hYZU3_.js";var t=a;export{t as default};
