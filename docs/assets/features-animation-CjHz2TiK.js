754d7291fedba201f271c29ee5ab15054897fb8e
mallgrab <mallgrab@a6:d9:bb:f5:da:61.home>
2026-06-11 14:43:36 +0200

    Publish: reservationForm: emails are now required

---
+++ docs/assets/features-animation-CjHz2TiK.js
@@ -0,0 +1,1 @@
+import{aL as a,aM as e,aN as n}from"./index-CJKN01jF.js";const s={renderer:n,...e,...a};export{s as d};
--- docs/assets/index-DD9v1yPO.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-DNE3zgog.js";import"./index-DO15xYAG.js";var d=a;export{d as default};
